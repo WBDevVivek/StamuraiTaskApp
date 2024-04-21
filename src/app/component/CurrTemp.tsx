57bad62805597203e761e8ef829b8272f76e2391
VIVEK GAWALE <126397466+WBDevVivek@users.noreply.github.com>
2024-04-21 12:32:07 +0530

    Update CurrTemp.tsx

--- src/app/component/CurrTemp.tsx
+++ src/app/component/CurrTemp.tsx
@@ -31,11 +31,22 @@ const CurrTemp: React.FC = ({}): React.JSX.Element => {
   }, [cityNameData])
 
 
+
+
+  const cityName = cityNameData?.name.replace(/ā/g, "a").replace(/ē/g, "e").replace(/ī/g, "i").replace(/ō/g, "o").replace(/ū/g, "u")
+
+
+
+
+
+
+
+  
   return (
     cityNameData !== undefined ? <section className={`flex w-[98%] md:w-[47%] lg:w-full   h-full  lg:h-1/2 md:mr-0 mb-4 md:mb-0  lg:mb-4 rounded-3xl justify-evenly bg-darkContaintColor`}>
 
       <div className={` ${flexColJustEveItemsCen}  h-full `}>
-        <h3 className={`text-xl xxsm:text-2xl  font-bold self-start`} >{cityNameData?.name}</h3>
+        <h3 className={`text-xl xxsm:text-2xl  font-bold self-start`} >{cityName}</h3>
         <p className={``} >{cityNameDataWeather?.description}</p>
         <h1 className={`text-2xl  xxsm:text-4xl font-bold`} >{cityNameData?.main?.temp}<sup>o</sup><b>C</b></h1>
       </div>
